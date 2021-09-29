df6cb94887a25b5adde8b1b59c661f6e22e1dc31
Kai Fricke <kai@anyscale.com>
2021-09-29 13:16:58 +0100

    Test with variants

--- examples/tft_iris_example/model_training_pipeline.py
+++ examples/tft_iris_example/model_training_pipeline.py
@@ -7,18 +7,22 @@ import numpy as np
 from tensorflow.keras.layers import Input, Dense
 from tensorflow.keras import Model
 
-TFT_OUTPUT_DIRECTORY = 'data/output/transform_artfcts/'
-TFRECORD_PATH = 'data/output/preprocessed_data/'
+# VARIANT = "with_actors"
+VARIANT = "with_tasks"
+# VARIANT = "with_directrunner"
+
+TFT_OUTPUT_DIRECTORY = f"data/output/{VARIANT}/transform_artfcts/"
+TFRECORD_PATH = f"data/output/{VARIANT}/preprocessed_data/"
 
 
 def _parse_function(proto, fs):
     parsed_features = tf.io.parse_single_example(proto, fs)
     return [
-        parsed_features['petal_length_normalized'],
-        parsed_features['petal_width_normalized'],
-        parsed_features['sepal_length_normalized'],
-        parsed_features['sepal_width_normalized']
-    ], parsed_features['target']
+        parsed_features["petal_length_normalized"],
+        parsed_features["petal_width_normalized"],
+        parsed_features["sepal_length_normalized"],
+        parsed_features["sepal_width_normalized"]
+    ], parsed_features["target"]
 
 
 def get_parse_function(tft_output_dir):
@@ -46,13 +50,13 @@ dataset = load_dataset(
 def build_model(input_shape=4, validation_split=0.1):
     tf.keras.backend.one_hot
     input_layer = Input(shape=(4, ))
-    hidden_layer = Dense(64, activation='relu')
+    hidden_layer = Dense(64, activation="relu")
     hidden_output = hidden_layer(input_layer)
     output_layer = Dense(3, activation="softmax")
     output = output_layer(input_layer)
     model = Model(inputs=input_layer, outputs=output)
     model.compile(
-        loss='categorical_crossentropy', optimizer='adam', metrics="accuracy")
+        loss="categorical_crossentropy", optimizer="adam", metrics="accuracy")
     return model
 
 
@@ -60,20 +64,20 @@ model = build_model()
 _ = model.fit(dataset, epochs=100, verbose=False)
 
 input_args = {
-    'sepal_length': np.array([[5.1]]),
-    'sepal_width': np.array([[3.5]]),
-    'petal_length': np.array([[1.4]]),
-    'petal_width': np.array([[0.2]]),
+    "sepal_length": np.array([[5.1]]),
+    "sepal_width": np.array([[3.5]]),
+    "petal_length": np.array([[1.4]]),
+    "petal_width": np.array([[0.2]]),
 }
 
 signature_dict = {
-    'petal_width': [
+    "petal_width": [
         tf.TensorSpec(shape=[], dtype=tf.float32, name="petal_width")
     ],
     "petal_length": [
         tf.TensorSpec(shape=[], dtype=tf.float32, name="petal_length")
     ],
-    'sepal_width': [
+    "sepal_width": [
         tf.TensorSpec(shape=[], dtype=tf.float32, name="sepal_width")
     ],
     "sepal_length": [
@@ -94,10 +98,10 @@ class MyModel(tf.keras.Model):
 
         modelInput = tf.stack(
             [
-                transformed['petal_length_normalized'],
-                transformed['petal_width_normalized'],
-                transformed['sepal_length_normalized'],
-                transformed['sepal_width_normalized']
+                transformed["petal_length_normalized"],
+                transformed["petal_width_normalized"],
+                transformed["sepal_length_normalized"],
+                transformed["sepal_width_normalized"]
             ],
             axis=1)
         pred = self.model(modelInput)
@@ -108,4 +112,4 @@ myModel = MyModel(model, TFT_OUTPUT_DIRECTORY)
 
 myModel.predict(input_args)
 
-myModel.save("data/output/saved_model")
+myModel.save(f"data/output/{VARIANT}/saved_model")

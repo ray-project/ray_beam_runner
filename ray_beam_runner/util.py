0f2c9ff95ffc3fdfe04b9b4c5d26da192d9d8045
Jiajun Yao <jeromeyjj@gmail.com>
2021-10-28 08:14:50 -0700

    Replace pandas groupby with dataset groupby

--- ray_beam_runner/util.py
+++ ray_beam_runner/util.py
@@ -1,32 +1,6 @@
 import ray
 
-import pandas as pd
-from apache_beam.transforms.window import GlobalWindow, WindowedValue
 from apache_beam.pipeline import PipelineVisitor
-
-
-def group_by_key(ray_ds: ray.data.Dataset):
-    df = pd.DataFrame()
-    for windowed_value in ray_ds.iter_rows():
-        if not isinstance(windowed_value, WindowedValue):
-            windowed_value = WindowedValue(windowed_value, 0,
-                                           (GlobalWindow(), ))
-
-        # Extract key from windowed value
-        key, value = windowed_value.value
-
-        # We convert to strings here to support void keys
-        key = str(key) if key is None else key
-        df = df.append([[key, value]])
-
-    if df.empty:
-        return {}
-
-    # part[0] is the key
-    # part[1][1] returns the (windowed) value
-    groups = {part[0]: list(part[1][1]) for part in df.groupby(0, sort=False)}
-    return groups
-
 
 class PipelinePrinter(PipelineVisitor):
     def visit_value(self, value, producer_node):

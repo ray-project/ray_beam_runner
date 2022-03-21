6afc5221a365aed59587c536a5cc7b9eb78d794a
Pablo Estrada <pabloem@apache.org>
2022-03-21 13:00:40 -0700

    fixup

--- ray_beam_runner/portability/execution.py
+++ ray_beam_runner/portability/execution.py
@@ -15,7 +15,7 @@
 # limitations under the License.
 #
 
-"""Set of utilities for execution of a pipeline by the FnApiRunner."""
+"""Set of utilities for execution of a pipeline by the RayRunner."""
 
 # mypy: disallow-untyped-defs
 

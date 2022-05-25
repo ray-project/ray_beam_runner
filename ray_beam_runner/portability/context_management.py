817c3304f00dfe6867958632a954f96d719654db
Patrick Ames <smtp.pdames@gmail.com>
2022-05-25 11:00:28 -0700

    Update ray_beam_runner/portability/context_management.py

--- ray_beam_runner/portability/context_management.py
+++ ray_beam_runner/portability/context_management.py
@@ -14,7 +14,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-import logging
 import typing
 from typing import List
 from typing import Optional

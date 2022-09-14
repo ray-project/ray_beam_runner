27dab9142341e80638c23969cb032df1aad6c327
github-action-benchmark <github@users.noreply.github.com>
2022-09-14 04:31:51 +0000

    add Python Benchmark with pytest-benchmark (pytest) benchmark result for 52475eb3adb230c80e1149b537ed807c03866fea

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1660709782223,
+  "lastUpdate": 1663129911159,
   "repoUrl": "https://github.com/ray-project/ray_beam_runner",
   "entries": {
     "Python Benchmark with pytest-benchmark": [
@@ -27,6 +27,33 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 8.63711916609269e-8",
             "extra": "mean: 111.14661106717445 nsec\nrounds: 23981"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "ray-project",
+            "username": "ray-project"
+          },
+          "committer": {
+            "name": "ray-project",
+            "username": "ray-project"
+          },
+          "id": "52475eb3adb230c80e1149b537ed807c03866fea",
+          "message": "[WIP]add benchmark for performance testing",
+          "timestamp": "2022-09-13T18:00:58Z",
+          "url": "https://github.com/ray-project/ray_beam_runner/pull/36/commits/52475eb3adb230c80e1149b537ed807c03866fea"
+        },
+        "date": 1663129910823,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "benchmark/simple.py::test_simple_benchmark",
+            "value": 11456008.726675073,
+            "unit": "iter/sec",
+            "range": "stddev: 1.2537819684740362e-8",
+            "extra": "mean: 87.29043629931957 nsec\nrounds: 57472"
           }
         ]
       }
